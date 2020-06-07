f41b788032fc5af84b3d66909cde4410717e5bcd
Jyotiranjan <jsatapathy007@gmail.com>
2020-06-07 11:57:48 +0530

    Create tic_toc_toe_numeric.py
    Number based tic-toc-toe with twoplyers

---
+++ tic_toc_toe_numeric.py
@@ -0,0 +1,79 @@
+board = [0, 1, 2,
+         3, 4, 5,
+         6, 7, 8]
+boardLog = [0, 0, 0,
+            0, 0, 0,
+            0, 0, 0]
+#players turn
+player = 'a'
+
+def tic_tac_toe ():
+    print ('|' ,board[0],'|',board[1] ,'|', board[2],'|')
+    print ('--------------------')
+    print ('|' ,board[3],'|',board[4] ,'|', board[5],'|')
+    print ('--------------------')
+    print ('|' ,board[6],'|',board[7] ,'|', board[8],'|')
+
+def move(x1,x2):
+    board[x2] = x1
+    boardLog[x2] = 1
+    tic_tac_toe()
+
+def odd (x, x2):
+    while  (x%2==0):
+        x = int(input ('enter an odd number'))
+    #not checking numbers out of range
+    move (x ,x2)      
+
+def even (x ,x2) :
+    while  (x%2!=0):
+        x = int(input ('enter an even number'))
+    #Same here
+    move (x ,x2)        
+
+def winner():
+    if (boardLog[0] + boardLog[1] + boardLog[2] == 3):
+      if (board[0]+board [1]+board[2]==15):
+          print ('you are the winner')
+          return True
+    if (boardLog[0] + boardLog[3] + boardLog[6] == 3):
+      if (board[0]+board [3]+board[6]==15):
+          print ('you are the winner')
+          return True
+    if (boardLog[1] + boardLog[4] + boardLog[7] == 3):
+      if (board[1]+board [4]+board[7]==15):
+          print ('you are the winner')
+          return True
+    if (boardLog[3] + boardLog[4] + boardLog[5] == 3):
+      if (board[3]+board [4]+board[5]==15):
+          print ('you are the winner')
+          return True
+    if (boardLog[2] + boardLog[5] + boardLog[8] == 3):
+      if (board[2]+board [5]+board[8]==15):
+          print ('you are the winner')
+          return True
+    if (boardLog[6] + boardLog[7] + boardLog[8] == 3):
+      if (board[6]+board [7]+board[8]==15):
+          print ('you are the winner')
+          return True
+
+    else: return False
+
+def turn(s):
+    print ('its '+ s +' turn')
+    x = int (input ('enter the number: '))
+    x1 = int (input ('enter the places number: '))
+    if player == 'a':
+        even(x, x1)
+    else: odd(x, x1)          
+
+print('Tic Tac Toe')
+print ('player A should enter even numbers only'+' and player B should enter odd numbers only')
+print ('the player with the ood numbers start')
+tic_tac_toe ()
+def numericTicTocToe():
+     while (True):
+        turn(player)
+        if winner(): break
+        else:
+            if player == 'a': player = 'b'
